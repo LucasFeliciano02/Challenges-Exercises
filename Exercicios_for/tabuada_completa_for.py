16d61401e5e3f8637ea04e71b0618421e0835c3f
unknown <LucasFeliciano02@outlook.com>
2022-04-23 08:27:31 -0300

    Tabuada completa com laço for

---
+++ Exercicios_for/tabuada_completa_for.py
@@ -0,0 +1,5 @@
+
+for i in range(1, 11):
+    print(f'==========[Tabuada {i}]==========')  # A CADA 10 multiplicações ELE MOSTRA ISSO
+    for j in range(1, 11):
+        print(f'{i} X {j} = {i*j}')
