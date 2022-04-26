4c2a83ca10f2b88982bedf8467bd2b25d8f8c37d
unknown <LucasFeliciano02@outlook.com>
2022-04-26 10:04:13 -0300

    Exercicio que responde qual seu genero sexual

---
+++ Exercicios_if_elif_else/estrutura_de_decisao_Sexo.py
@@ -0,0 +1,10 @@
+sexo = input('Digite seu Sexo M ou F: ')
+
+
+if sexo.upper() == 'M':
+    print('Masculino')
+elif sexo.upper() == 'F':
+    print('Feminino')
+else:
+    print('Valor inválido')
+    
